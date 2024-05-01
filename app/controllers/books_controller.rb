61575ba1472e95b62b1d9fffe13c7ebe6cb0e67f
daikitakaki <162309554+takakidaiki@users.noreply.github.com>
2024-05-01 06:01:12 +0000

    [finish]1

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  private
+
+  def post_image_params
+    params.require(:book).permit(:title, :body)
+  end
+end
