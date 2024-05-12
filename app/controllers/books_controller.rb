60e8fbe2fb4e40de6f2c6bec471a339f8045c233
daikitakaki <162309554+takakidaiki@users.noreply.github.com>
2024-05-12 01:16:10 +0000

    9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,9 +16,17 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
