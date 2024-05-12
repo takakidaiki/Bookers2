be542fee10958a0ab7cff1e2e615b7d6b30b285b
daikitakaki <162309554+takakidaiki@users.noreply.github.com>
2024-05-12 09:00:01 +0000

    [finish]9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,24 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all 
+    @users = User.all
     @user=current_user
-  end 
-  
+  end
+
   def show
-    @user =　User.find(params[:id])
+    @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
-  
+
   private
 
   def user_params
