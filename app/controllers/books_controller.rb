0a092cc5a415a69e7ee1b438a49e993505af49f4
daikitakaki <162309554+takakidaiki@users.noreply.github.com>
2024-05-14 12:50:51 +0000

    [finish]11

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,6 +29,7 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def destroy
@@ -38,10 +39,10 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "You have updated book successfully."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
